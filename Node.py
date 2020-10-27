42990a6be389227a4a6bb472ae2dba52b66f7e4b
dkncus <dkncus@gmail.com>
2020-10-27 11:09:55 -0400

    Updated to include Directed Acyclic Graphs

--- Node.py
+++
@@ -1,6 +0,0 @@
-class Node:
-    #Create a new node
-    def __init__(self, nodeKey):
-        self.l = None
-        self.r = None
-        self.nodeKey = nodeKey  #Assign the value of the Node to this Node
--- test_LCA.py
+++ test_LCA.py
@@ -1,68 +1,45 @@
 from unittest import TestCase
-
-from Node import Node
-from main import findLowestCommonAncestor, getPathToNodeFromRoot
+from main import findLowestCommonAncestor
+import networkx as nx
 
 class Test(TestCase):
     def test_find_lowest_common_ancestor(self):
         # Hard Coded Binary Tree
-        root = Node(0)
-        root.l = Node(1)
-        root.r = Node(2)
-        root.l.l = Node(3)
-        root.l.r = Node(4)
-        root.r.l = Node(5)
-        root.r.r = Node(6)
-        root.l.l.l = Node(7)
-        root.l.l.r = Node(8)
-        root.l.r.l = Node(9)
-        root.l.r.r = Node(10)
-        root.r.l.l = Node(11)
-        root.r.l.r = Node(12)
-        root.r.r.l = Node(13)
-        root.r.r.r = Node(14)
+        graph = nx.DiGraph()
+        graph_objects = [   ("root", 0),
+                            (0, 1),
+                            (0, 2),
+                            (1, 3),
+                            (1, 4),
+                            (2, 5),
+                            (2, 6),
+                            (3, 7),
+                            (3, 8),
+                            (4, 9),
+                            (4, 10),
+                            (5, 11),
+                            (5, 12),
+                            (6, 13),
+                            (6, 14),
+                            (7, 18),
+                            (9, 15),
+                            (11, 15),
+                            (12, 16),
+                            (14, 16),
+                            (15, 17),
+                            (16, 17),
+                            (17, 18)]
+
+        graph.add_edges_from(graph_objects)
 
         # LCA Assertions
-        self.assertEqual(findLowestCommonAncestor(root, 11, 13), 2)
-        self.assertEqual(findLowestCommonAncestor(root, 9, 10), 4)
-        self.assertEqual(findLowestCommonAncestor(root, 3, 4), 1)
-        self.assertEqual(findLowestCommonAncestor(root, 7, 8), 3)
-        self.assertEqual(findLowestCommonAncestor(root, 11, 12), 5)
+        self.assertEqual(findLowestCommonAncestor(graph, 11, 13), 2)
+        self.assertEqual(findLowestCommonAncestor(graph, 9, 10), 4)
+        self.assertEqual(findLowestCommonAncestor(graph, 3, 4), 1)
+        self.assertEqual(findLowestCommonAncestor(graph, 7, 8), 3)
+        self.assertEqual(findLowestCommonAncestor(graph, 11, 12), 5)
+        self.assertEqual(findLowestCommonAncestor(graph, 17, 7), 1)
+        self.assertEqual(findLowestCommonAncestor(graph, 16, 15), 5)
+        self.assertEqual(findLowestCommonAncestor(graph, 17, 14), 14)
 
         print("LCA Tests Passed")
-
-    def test_get_path_to_node_from_root(self):
-        #Hard Coded Binary Tree
-        root = Node(0)
-        root.l = Node(1)
-        root.r = Node(2)
-        root.l.l = Node(3)
-        root.l.r = Node(4)
-        root.r.l = Node(5)
-        root.r.r = Node(6)
-        root.l.l.l = Node(7)
-        root.l.l.r = Node(8)
-        root.l.r.l = Node(9)
-        root.l.r.r = Node(10)
-        root.r.l.l = Node(11)
-        root.r.l.r = Node(12)
-        root.r.r.l = Node(13)
-        root.r.r.r = Node(14)
-
-        path = []
-        getPathToNodeFromRoot(root, path, 13)
-        self.assertEqual(path, [0, 2, 6, 13])
-
-        path = []
-        getPathToNodeFromRoot(root, path, 5)
-        self.assertEqual(path, [0, 2, 5])
-
-        path = []
-        getPathToNodeFromRoot(root, path, 11)
-        self.assertEqual(path, [0, 2, 5, 11])
-
-        path = []
-        getPathToNodeFromRoot(root, path, 7)
-        self.assertEqual(path, [0, 1, 3, 7])
-
-        print("GetPath test passed")
